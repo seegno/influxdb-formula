7f91e1fa2a78ac69a366f1a295a0276557156f4d
Alfredo Palhares <masterkorp@masterkorp.net>
2014-08-07 12:27:05 +0100

    Remove RSpec bin path configure loop from the ServerSpec test

--- test/integration/default/serverspec/influxdb_spec.rb
+++ test/integration/default/serverspec/influxdb_spec.rb
@@ -3,10 +3,6 @@ require "serverspec"
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
-RSpec.configure do |c|
-  c.before :all do
-    c.path = "/sbin:/usr/sbin"
-  end
 end
 
 describe "InfluxDB Server" do
